4a693c30d3a3e22a54c1f2765201b66331118d5c
DrkSephy <dleonar00@citymail.cuny.edu>
2016-03-18 15:14:27 -0400

    Converting tabs to spaces to preserve 2 space indentation

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	entry: './js/main.js',
-	output: {
-		path: path.join(__dirname, 'js'),
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'babel-loader',
-				test: path.join(__dirname, 'js'),
-				query: {
-					presets: 'es2015',
-				},
-			}
-		]
-	},
-	plugins: [
-		// Avoid publishing files when compilation fails
-		new webpack.NoErrorsPlugin()
-	],
-	stats: {
-		// Nice colored output
-		colors: true
-	},
-	// Create Sourcemaps for the bundle
-	devtool: 'source-map',
+  entry: './js/main.js',
+  output: {
+    path: path.join(__dirname, 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: path.join(__dirname, 'js'),
+        query: {
+          presets: 'es2015',
+        },
+      }
+    ]
+  },
+  plugins: [
+    // Avoid publishing files when compilation fails
+    new webpack.NoErrorsPlugin()
+  ],
+  stats: {
+    // Nice colored output
+    colors: true
+  },
+  // Create Sourcemaps for the bundle
+  devtool: 'source-map',
 };
