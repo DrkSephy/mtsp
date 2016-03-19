f0109154be3539925ac5f9301c90520025e92e4b
DrkSephy <dleonar00@citymail.cuny.edu>
2016-03-19 13:44:02 -0400

    Implemented method for evolving population using genetic operators

--- js/genetic.js
+++ js/genetic.js
@@ -11,4 +11,31 @@ class Genetic {
     this.tournamentSize = 5;
     this.elitism = true;
   }
+
+  /**
+   * Evolves a population using Genetic Operators.
+   * @param {object} population - Instance of Population class.
+   * @returns {array} newPopulation - The new generation of individuals.
+  */
+  evolvePopulation(population) {
+    let newPopulation = Population(this.destinations, population.populationSize(), false);
+    let elitismOffset = 0;
+    if (this.elitism) {
+      newPopulation.saveTrip(0, population.getFittest());
+      elitismOffset = 1;
+    }
+
+    for (var i = elitismOffset; i < newPopulation.populationSize(); i++) {
+      let parent1 = this.tournamentSelection(population);
+      let parent2 = this.tournamentSelection(population);
+      let child   = this.crossover(parent1, parent2);
+      newPopulation.saveTrip(i, child);
+    }
+
+    for (var i = elitismOffset; i < newPopulation.populationSize(); i++) {
+      this.mutate(newPopulation.getTrip(i));
+    }
+
+    return newPopulation;
+  }
 }
