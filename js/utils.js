c60bc2b92249a2089046ebf6b2b46ad547d6f962
DrkSephy <dleonar00@citymail.cuny.edu>
2016-03-19 11:24:45 -0400

    Implemented utility method for checking if object is in array

--- js/utils.js
+++ js/utils.js
@@ -21,11 +21,26 @@ export function generateRandomInt(min, max) {
  * @returns {array} array - Shuffled input array.
 */
 export function shuffleArray(array) {
-  for(var i = array.length - 1; i > 0; i--) {
+  for (var i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     let temp = array[i];
     array[i] = array[j];
     array[j] = temp;
   }
   return array;
+}
+
+/**
+ * Checks if an object is inside an array.
+ * @param {object} obj - The object to test inclusion.
+ * @param {array} array - The array to test object inclusion.
+ * @returns {boolean} Whether the array contained the given object.
+*/
+export function containsObject(obj, array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === obj) {
+      return true;
+    }
+  }
+  return false;
 }
