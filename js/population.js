75ce03283a21be295cff2d393f60086d948bb8a4
DrkSephy <dleonar00@citymail.cuny.edu>
2016-03-19 11:57:42 -0400

    Implemented saveTrip method for Population class

--- js/population.js
+++ js/population.js
@@ -1,10 +1,26 @@
 'use strict';
 
 class Population {
+  /**
+   * Manages our population of trips.
+   * @param {array} destinations - Instance of Destinations class.
+   * @param {number} size - The size of the population to generate.
+   * @param {boolean} initialize - Whether or not to initialize a new population.
+  */
   constructor(destinations, size, initialize) {
     this.trips = [];
     for(var i = 0; i <= size; i++) {
       this.trips.push(null);
     }
+  }
+
+  /**
+   * Saves a trip.
+   * @param {number} key - The population index to save trip to.
+   * @param {number} value - The trip to save.
+   * @returns {undefined}
+  */
+  saveTrip(key, value) {
+    this.tours[key] = value;
   }
 }
