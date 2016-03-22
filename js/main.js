c087fcac1524737a6c98d412b37bbd896fcdb18b
DrkSephy <dleonar00@citymail.cuny.edu>
2016-03-21 21:03:55 -0400

    Procedurally generating cities using random number generator

--- js/main.js
+++ js/main.js
@@ -8,51 +8,18 @@ import Genetic from './genetic.js';
 import Population from './population.js';
 import Simulation from './simulation.js';
 import Destinations from './destinations.js';
+import {generateUniqueNumbers} from './utils.js';
 
 // Store destination cities
 let destinations = new Destinations();
 
-// Add some cities
-let city = new City(60, 200)
-destinations.addCity(city)
-let city2 = new City(180, 200)
-destinations.addCity(city2)
-let city3 = new City(80, 180)
-destinations.addCity(city3)
-let city4 = new City(140, 180)
-destinations.addCity(city4)
-let city5 = new City(20, 160)
-destinations.addCity(city5)
-let city6 = new City(100, 160)
-destinations.addCity(city6)
-let city7 = new City(200, 160)
-destinations.addCity(city7)
-let city8 = new City(140, 140)
-destinations.addCity(city8)
-let city9 = new City(40, 120)
-destinations.addCity(city9)
-let city10 = new City(100, 120)
-destinations.addCity(city10)
-let city11 = new City(180, 100)
-destinations.addCity(city11)
-let city12 = new City(60, 80)
-destinations.addCity(city12)
-let city13 = new City(120, 80)
-destinations.addCity(city13)
-let city14 = new City(180, 60)
-destinations.addCity(city14)
-let city15 = new City(20, 40)
-destinations.addCity(city15)
-let city16 = new City(100, 40)
-destinations.addCity(city16)
-let city17 = new City(200, 40)
-destinations.addCity(city17)
-let city18 = new City(20, 20)
-destinations.addCity(city18)
-let city19 = new City(60, 20)
-destinations.addCity(city19)
-let city20 = new City(160, 20)
-destinations.addCity(city20);
+// Generate destination cities
+let xCoordinates = generateUniqueNumbers(50, 450);
+let yCoordinates = generateUniqueNumbers(50, 450);
+for (var j = 0; j < 50; j++) {
+  let city = new City(xCoordinates[j], yCoordinates[j]);
+  destinations.addCity(city);
+}
 
 // Setup Simulation class and event handlers
 let simulation = new Simulation();
@@ -65,7 +32,7 @@ let draw = new Draw();
 let stats = new Stats();
 
 // Generate the initial population
-let population = new Population(destinations, 50, true);
+let population = new Population(destinations, 30, true);
 stats.setStartingDistance(population.getFittest().computeDistance());
 stats.showStartingDistance();
 stats.setOptimalDistance();
@@ -76,7 +43,7 @@ let ga = new Genetic(destinations);
 population = ga.evolvePopulation(population);
 
 // Evolve over 100 generations
-for (var x = 0; x < 100; x++) {
+for (var x = 0; x < 200; x++) {
   (function(delay) {
     setTimeout(function() {
       population = ga.evolvePopulation(population);
@@ -122,3 +89,4 @@ for (var x = 0; x < 100; x++) {
     }
   })(x);
 }
+
